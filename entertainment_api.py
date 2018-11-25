2320ebcc86f076956d02437be1ca988e2363c6cb
okaysidd <siddhartha.sehgal94@gmail.com>
2018-11-25 20:15:21 +0530

    Made changes to entertainment.api file with proper formatting

--- entertainment_api.py
+++ entertainment_api.py
@@ -5,97 +5,153 @@ import json
 import movies
 import webbrowser
 
+
 tmdb.API_KEY = '9ee394eab0a844afcd7dbf8af77bded5'
 
-with open('movies_details.json','r') as file:
+
+with open('movies_details.json', 'r') as file:
     json_movies_data = json.load(file)
+
 
 # to save all movie titles to inform user if adding some movie twice
 movie_titles = []
 for i in range(len(json_movies_data['movies'])):
 	movie_titles.append(json_movies_data['movies'][i]['movie_title'])
 
-#---------------------------------------------------------------
-# displaying all the current movies' details in the json file
+
 def show_movies():
-    print('The movies website currently has {} movies. The following:\n'.format(len(json_movies_data['movies'])))
+    """
+    Displays all the movies.
+
+    Prints all the movies that are currently available in the system.
+    To let the user decide if they want to add more movies or not.
+    """
+    print('The movies website currently has {} movies.')
+    print('The following:\n'.format(len(json_movies_data['movies'])))
     for i in range(len(json_movies_data['movies'])):
-        print('{}. {}'.format(i+1, json_movies_data['movies'][i]['movie_title']))
+        title_of_movie = json_movies_data['movies'][i]['movie_title']
+        print('{}. {}'.format(i+1, title_of_movie))
 
 
-#---------------------------------------------------------------
-# let user add new movies if needed
 def search_for_movie():
+    """
+    Add new movies.
 
-	while True:
-		movie_name = input('\nWhich movie should we add to the trailers webpage?\n')
-		# initiating search method
-		search = tmdb.Search()
-		response = search.movie(query=movie_name)
-		movies_list = search.results
+    Gives option to user to add new movies.
+    If they want to add, looks up the movies mentioned using the tmdb API
+    and returns the top 3 results with their names and the date of release.
+    Else goes ahead with displaying the movies webpage in the next function.
 
-		if len(movies_list) == 0:
-			print('No such movie found :(')
-			print('Lets try again')
-			continue
-		# if only one search result with the movie name
-		if len(movies_list) == 1:
-			print('Is this the movie you meant? {} - release date- {}'.format(movies_list[0]['title'], movies_list[0]['release_date']))
-			movie_picked = input('Y/N  ').lower()
-			if movie_picked == 'yes' or movie_picked == 'yus' or movie_picked == 'y': # check for 'no' response as well
-				movie_picked = movies_list[0]['id']
-				return movie_picked
+    Returns
+    -----------------
+    movie object
+        returned from the API and contains movie details,
+        such as title, id and release date
 
-		# if search returned more than one result for the movie name
-		number=1
-		print('\nWhich movie did you mean?')
-		print()
-		for s in movies_list[:3]:
-			print('{}. {} -- release in {}'.format(str(number), s['title'], s['release_date']))
-			number+=1
-		print()
-		print('{}. None of these'.format(str(number)))
-		movie_choice = input('Enter a number..  ')
+    """
+    while True:
+        movie_name = input('\nWhich movie should we add to the trailers webpage?\n')
 
-		if movie_choice=='4':
-			continue
-		elif movie_choice=='':
-			print('That\'s an invalid input.')
-			continue
-		elif movie_choice in '123' and int(movie_choice) <= number:
-			movie_picked = movies_list[int(movie_choice)-1]
-			return movie_picked
-		else:
-			print('That\'s an invalid input.')
-			continue
+        # initiating search method
+        search = tmdb.Search()
+        response = search.movie(query=movie_name)
+        movies_list = search.results
 
-#---------------------------------------------------------------
-# get the youtube link of the movie selected from the api link
+        if len(movies_list) == 0:
+            print('No such movie found :(')
+            print('Lets try again')
+            continue
+
+        # if only one search result with the movie name
+        if len(movies_list) == 1:
+            print('Is this the movie you meant?')
+            title_of_movie = movies_list[0]['title']
+            release_date = movies_list[0]['release_date']
+            print('{} - release date- {}'.format(title_of_movie, release_date))
+            movie_picked = input('Y/N  ').lower()
+            if movie_picked == 'yes' or movie_picked == 'yus' or movie_picked == 'y':
+                movie_picked = movies_list[0]
+                return movie_picked
+
+        # if search returned more than one result for the movie name
+        number = 1
+        print('\nWhich movie did you mean?')
+        print()
+        for s in movies_list[:3]:
+            title_of_movie = s['title']
+            release_date = s['release_date']
+            print('{}. {} -- release in {}'.format(str(number), title_of_movie, release_date))
+            number += 1
+        print()
+        print('{}. None of these'.format(str(number)))
+        movie_choice = input('Enter a number..  ')
+
+        if movie_choice == '4':
+            continue
+        elif movie_choice == '':
+            print('That\'s an invalid input.')
+            continue
+        elif movie_choice in '123' and int(movie_choice) <= number:
+            movie_picked = movies_list[int(movie_choice)-1]
+            return movie_picked
+        else:
+            print('That\'s an invalid input.')
+            continue
+
+
 def get_youtube_link(movie_picked):
+	"""
+    Get the YouTube link to the movie trailer.
 
-	# the api link to get the movie data
-	link = 'https://api.themoviedb.org/3/movie/'+ movie_picked + '/videos?api_key=9ee394eab0a844afcd7dbf8af77bded5&language=en-US'
-	source = urllib.request.urlopen(link)
+    The API result returned from the tbdb python module usually
+    does not have the YouTube trailer link in the parameters.
+    This funtion takes the movie id and uses the url for the API
+    to get the YouTube link to the trailer of the movie.
 
-	# getting the movie data in json format from the http request format that the above api returns
-	data = source.read().decode("utf-8")
-	jmovie_data = json.loads(data)
-	youtube_link_key = jmovie_data["results"][0]['key']
-	youtube_link = 'https://www.youtube.com/watch?v=' + youtube_link_key
-	return youtube_link
+    Parameters
+    ---------------------
+    arg1: string
+        id (in string format) of the movie of which the YouTube
+        trailer is required.
 
-#---------------------------------------------------------------
-# choice to save these movies permanently in the json file
+    Returns
+    ---------------------
+    string
+        key of the YouTube link that can be used to generate the
+        whole trailer link.
+    """
+    link = 'https://api.themoviedb.org/3/movie/' + movie_picked + '/videos?api_key=' + tmdb.API_KEY + '&language=en-US'
+    try:
+        source = urllib.request.urlopen(link)
+    except:
+        print('Something went wrong. Maybe check the internet connection?\n')
+
+    # getting the movie data in json format from the http request format that the above api returns
+    data = source.read().decode("utf-8")
+    jmovie_data = json.loads(data)
+    youtube_link_key = jmovie_data["results"][0]['key']
+    youtube_link = 'https://www.youtube.com/watch?v=' + youtube_link_key
+    return youtube_link
+
+
 def save_movies():
-    save_movies = input('Save these movies permanently? Y/N  ')
-    if save_movies=='yes' or save_movies=='yus' or save_movies=='y':
-        with open('movies_details.json', 'w') as file:
-            json.dump(json_movies_data, file)
+	"""
+	Option to save new movies in the system.
+
+	If the user has queried for new movies, it gives the option to add those movies to the JSON we maintain, so those movies show up everytime without needing to add them later.
+	"""
+	save_movies = input('Save these movies permanently? Y/N  ')
+	if save_movies=='yes' or save_movies=='yus' or save_movies=='y':
+		with open('movies_details.json', 'w') as file:
+			json.dump(json_movies_data, file)
 
 
-#---------------------------------------------------------------
-# creating Movies() objects of all the movies present in the movies_details.json file, then displaying
 def display_trailer_webpage():
+	"""
+	Display the final trailer webpage.
+
+	Reads the JSON file with old and new (if added) movies and prepares the webpage with trailers of all the movies.
+	"""
 	movies_list=[]
 	for i in range(len(json_movies_data['movies'])):
 	    name = json_movies_data['movies'][i]['movie_title']
@@ -112,9 +168,22 @@ def display_trailer_webpage():
 	ft.open_movies_page(movies_list)
 
 
-#---------------------------------------------------------------
-# main method
 def main():
+	"""
+	Main function to controll all other functions from one place.
+
+	Handles all functions at one place and lets users have option to add new movies.
+	If they add new movies, gives options to save new movies' data permanently.
+	Retrieves all the movies' data using pythn API module or API url.
+	Displays the movies trailer webpage.
+	"""
+	try:
+		source = urllib.request.urlopen('https://www.google.com/')
+	except:
+		print('Your internet connection might not be working.\nYou won\'t be able to add new movies if the internet connection doesn\'t work.\nThe movies webpage might not work as intended, and the links to the YouTube trailer won\'t work.')
+		input('Click enter to continue..  ')
+		display_trailer_webpage()
+		return
 
 	show_movies()
 
